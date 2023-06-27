efbdb97756d3cd9ac51ac117698b02daea9ed366
Jiayuning <1073967337@qq.com>
2023-06-27 11:17:43 +0800

    update files

---
+++ Springdemo/src/test/java/com/example/springdemo/testdemo/tmp.java
@@ -0,0 +1,16 @@
+package com.example.springdemo.testdemo;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @Author: Jia Yuning
+ * @Description:
+ * @Date: 2023/06/27 11:13:58
+ * @Version: 1.0.0
+ */
+public class tmp {
+    @Test
+    public void test1() {
+        System.out.println("123");
+    }
+}
---
+++ practice/src/tmp/t.java
@@ -0,0 +1,18 @@
+package tmp;
+
+/**
+ * @Author: Jia Yuning
+ * @Description:
+ * @Date: 2023/06/24 17:22:05
+ * @Version: 1.0.0
+ */
+public class t {
+    public static void main(String[] args) {
+
+    }
+
+    public String test(Integer num, String s) {
+        Integer num1 = num;
+        return new String();
+    }
+}
