d91c04581f2af0fd57136b1957a9fa6401514f54
Jiayuning <1073967337@qq.com>
2024-03-07 21:55:14 +0800

    加个模块

---
+++ firstProject/src/main/java/com/jyn/firstProject/config/TestEnvConfig.java
@@ -0,0 +1,19 @@
+package com.jyn.firstProject.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
+
+/**
+ * Profile("test")注解使得spring.properties.active = test 的时候才注入该配置Bean
+ * 即 JVM启动的Options参数加入 -Dspring.properties.active=test
+ *
+ * @author jiayn01
+ * @date 2023-10-20 18:03:07
+ */
+
+@Profile("test")
+@Configuration
+@PropertySource(value = {"classpath:envConfig/test/test.properties", "classpath:envConfig/test/http.properties"}, encoding = "utf-8")
+public class TestEnvConfig {
+}
---
+++ firstProject/src/main/java/com/jyn/firstProject/entities/vo/demo/DemoRequest.java
@@ -0,0 +1,16 @@
+package com.jyn.firstProject.entities.vo.demo;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * @Author: Jia Yuning
+ * @Description:
+ * @Date: 2024/03/04 22:03:17
+ * @Version: 1.0.0
+ */
+@Data
+public class DemoRequest implements Serializable {
+    private String id;
+}
